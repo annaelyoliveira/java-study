0af7ddb3bde207935353e340f71a30b8de7f518b
Annaely Oliveira <annaely691@gmail.com>
2024-05-09 19:35:06 -0300

    VerificacaoChequeEspecial

--- DesafioLimiteCheque/src/VerificacaoChequeEspecial.java
+++ DesafioLimiteCheque/src/VerificacaoChequeEspecial.java
@@ -5,18 +5,22 @@ public class VerificacaoChequeEspecial {
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in); 
 
+        System.out.println("Insira o valor do saldo:");
         double saldo = scanner.nextDouble(); 
+
+        System.out.println("Insira o valor do saque:");
         double saque = scanner.nextDouble(); 
+
         double limiteChequeEspecial = 500; 
 
         // TODO: Verifique se o saque não ultrapassa o saldo disponível na conta:
         if (saque < saldo){
-          System.out.println("Transacao realizada com sucesso.");
+          System.out.println("Transação realizada com sucesso.");
         } else {
           if(saque < (saldo+limiteChequeEspecial)){
-            System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+            System.out.println("Transação realizada com sucesso utilizando o cheque especial.");
           }else {
-            System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+            System.out.println("Transação não realizada. Limite do cheque especial excedido.");
           }
         } 
 
